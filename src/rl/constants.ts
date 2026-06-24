4ded933b155e5095a4adc25b044b1af256662053
Kalilamodow <kalilamodow@gmail.com>
2026-06-24 12:10:16 -0500

    update BUILD_ID constant

--- src/rl/constants.ts
+++ src/rl/constants.ts
@@ -2,4 +2,4 @@ export const PSY_KEY = "c338bd36fb8c42b1a431d30add939fc7";
 
 // if stale get these from rl's Launch.log
 export const FEATURE_SET = "PrimeUpdate59";
-export const BUILD_ID = "939334844";
+export const BUILD_ID = "102852329";
