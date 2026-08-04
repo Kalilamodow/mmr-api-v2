d6f0dee4da1912282b65dd49ad2022163cbcf1a3
Kalilamodow <kalilamodow@gmail.com>
2026-08-03 19:18:25 -0500

    new webadmin: only show from service in logs

--- src/webadmin/src/components/Logs.svelte
+++ src/webadmin/src/components/Logs.svelte
@@ -10,10 +10,18 @@
     type: LogType;
   };
   let logs = $state<LogEntry[] | null>(null);
+  let logCategories = $state<Record<string, boolean>>({});
 
   async function load() {
     const response = await fetchWithPassword("./api/logs");
     logs = (await response.json()).logs;
+    for (const log of logs!) {
+      logCategories[log.from] = true;
+    }
+  }
+
+  function filteredLogs(logs: LogEntry[]) {
+    return logs.filter((l) => logCategories[l.from]);
   }
 
   $effect(() => {
@@ -25,9 +33,22 @@
 
 {#if logs}
   <Cell name="Logs">
+    <div class="category-selector">
+      <strong>Only show from</strong>
+
+      {#each Object.keys(logCategories) as category}
+        <label for={category}>{category}</label>
+        <input
+          type="checkbox"
+          id={category}
+          bind:checked={logCategories[category]}
+        />
+      {/each}
+    </div>
+
     <table>
       <tbody>
-        {#each logs as log}
+        {#each filteredLogs(logs) as log}
           <tr class="log-{log.type}">
             <th>{log.type}</th>
             <td>{new Date(log.time).toLocaleString()}</td>
@@ -49,5 +70,19 @@
   tr.log-error {
     background-color: red;
     color: white;
+  }
+
+  div.category-selector {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 12px;
+  }
+
+  div.category-selector strong {
+    margin-right: 8px;
+  }
+
+  div.category-selector input[type="checkbox"] {
+    margin-right: 4px;
   }
 </style>
