849748ce7d16307ddd0275aecc33d823e9372a1e
Kalilamodow <kalilamodow@gmail.com>
2026-08-05 23:35:29 -0500

    chore: bump feature set/build id

--- src/rl/constants.ts
+++ src/rl/constants.ts
@@ -1,5 +1,5 @@
 export const PSY_KEY = "c338bd36fb8c42b1a431d30add939fc7";
 
 // if stale get these from rl's Launch.log
-export const FEATURE_SET = "PrimeUpdate59";
-export const BUILD_ID = "102852329";
+export const FEATURE_SET = "PrimeUpdate59_1";
+export const BUILD_ID = "2004174750";
